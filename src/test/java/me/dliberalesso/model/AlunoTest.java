10d625fd4a517ef31cd775cbdc92de3fe553d419
Douglas Liberalesso <dliberalesso@gmail.com>
2015-11-12 23:38:19 -0200

    Implementa testes para os métodos equals() e hashCode() de Aluno.

--- src/test/java/me/dliberalesso/model/AlunoTest.java
+++ src/test/java/me/dliberalesso/model/AlunoTest.java
@@ -3,6 +3,7 @@ package me.dliberalesso.model;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * @author Douglas Liberalesso
@@ -14,5 +15,23 @@ public class AlunoTest {
     @Test
     public void testToString() {
         assertEquals("[ID: 0] Douglas Liberalesso", aluno.toString());
+    }
+
+    @Test
+    public void testHashCode() {
+        int hashCode = (31 * aluno.getNome().hashCode() + aluno.getSobrenome().hashCode()) * 31 * 31;
+        assertEquals(hashCode, aluno.hashCode());
+    }
+
+    @Test
+    public void testIsEqual() {
+        Aluno aluno1 = new Aluno("Douglas", "Liberalesso");
+        assertEquals(aluno1, aluno);
+    }
+
+    @Test
+    public void testNotEqual() {
+        Aluno aluno1 = new Aluno("Douuglas", "Liberalesso");
+        assertNotEquals(aluno1, aluno);
     }
 }
