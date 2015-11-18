4ab31d39eafd68655eacbbe25de32263e22a2fca
Douglas Liberalesso <dliberalesso@gmail.com>
2015-11-18 15:32:31 -0200

    Remove Listeners, pois não funcionam com SessionFactory

--- src/main/java/me/dliberalesso/model/BaseEntity.java
+++ src/main/java/me/dliberalesso/model/BaseEntity.java
@@ -1,8 +1,6 @@
 package me.dliberalesso.model;
 
 
-import me.dliberalesso.listener.Listener;
-
 import javax.persistence.*;
 import java.util.Date;
 
@@ -11,7 +9,6 @@ import java.util.Date;
  */
 
 @MappedSuperclass
-@EntityListeners(Listener.class)
 public abstract class BaseEntity {
     private int id;
     private Date createdAt;
