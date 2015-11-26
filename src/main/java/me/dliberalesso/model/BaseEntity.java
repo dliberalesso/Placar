ab1ea3b0845d8fb546b341c982cdf5390b5aee31
Douglas Liberalesso <dliberalesso@gmail.com>
2015-11-26 14:17:20 -0200

    Cria a documentacao para a o pacote Model e suas classes

--- src/main/java/me/dliberalesso/model/BaseEntity.java
+++ src/main/java/me/dliberalesso/model/BaseEntity.java
@@ -8,9 +8,9 @@ import java.util.Date;
 
 /**
  * Define atributos basicos e necessarios para todas as Entidades que compoem o Modelo.
- *
- * <p>Esta classe contem um conjunto de <i>setters</i> e <i>getters</i> e estabelece nomes
- * apropriados para as colunas no Banco de Dados.</p>
+ * <p>
+ * Esta classe contem um conjunto de <i>setters</i> e <i>getters</i> e estabelece nomes
+ * apropriados para as colunas no Banco de Dados.
  */
 
 @MappedSuperclass
@@ -19,6 +19,11 @@ public abstract class BaseEntity {
     private Date createdAt;
     private Date updatedAt;
 
+    /**
+     * Retorna o identificador da entidade junto ao Banco de Dados.
+     *
+     * @return id
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
@@ -26,36 +31,69 @@ public abstract class BaseEntity {
         return id;
     }
 
+    /**
+     * Seta o ID da entidade junto ao Banco de Dados.
+     * @param id Atributo identificador
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Retorna a data em que a entidade foi salva pela primeira vez no Banco de Dados.
+     * @return createdAt
+     */
     @Column(name = "created_at")
     @CreationTimestamp
     public Date getCreatedAt() {
         return createdAt;
     }
 
+    /**
+     * Seta a data em que a entidade foi salva pela primeira vez no Banco de Dados.
+     * @param createdAt Data em que a entidade foi criada
+     */
     public void setCreatedAt(Date createdAt) {
         this.createdAt = createdAt;
     }
 
+    /**
+     * Retorna a data em que a entidade foi modificada pela ultima vez no Banco de Dados.
+     * @return updatedAt
+     */
     @Column(name = "updated_at")
     @UpdateTimestamp
     public Date getUpdatedAt() {
         return updatedAt;
     }
 
+    /**
+     * Seta a data em que a entidade foi modificada pela ultima vez no Banco de Dados.
+     * @param updatedAt Data em que a entidade foi modificada
+     */
     public void setUpdatedAt(Date updatedAt) {
         this.updatedAt = updatedAt;
     }
 
+    /**
+     * Retorna um booleano que representa a igualdade entre os objetos.
+     * @param o Objeto com o qual se deseja comparar
+     * @return true or false
+     */
     @Override
     public abstract boolean equals(Object o);
 
+    /**
+     * Retorna o hashcode representativo do objeto.
+     * @return hashCode
+     */
     @Override
     public abstract int hashCode();
 
+    /**
+     * Retorna a representação do objeto em uma String.
+     * @return String
+     */
     @Override
     public abstract String toString();
 }
--- src/main/java/me/dliberalesso/model/package-info.java
+++ src/main/java/me/dliberalesso/model/package-info.java
@@ -2,11 +2,11 @@
  * Define classes com os dados utilizando o padrao <a href="https://pt.wikipedia.org/wiki/JavaBeansBEAN">JavaBeans</a>
  * que irao mapear uma tabela do Banco de Dados.
  * <p>
- * <p>Nomeado a partir da convencao <a href="https://pt.wikipedia.org/wiki/MVC">MVC</a>,
- * este pacote contem as classes que sao referentes as entidades/tabelas do Banco de Dados.</p>
+ * Nomeado a partir da convencao <a href="https://pt.wikipedia.org/wiki/MVC">MVC</a>,
+ * este pacote contem as classes que sao referentes as entidades/tabelas do Banco de Dados.
  * <p>
- * <p>As classes seguem o padrao de get/set para cada atributo(coluna) do Banco de Dados e, como
- * convencao, estao anotadas como <i>Entity</i>.</p>
+ * As classes seguem o padrao de get/set para cada atributo(coluna) do Banco de Dados e, como
+ * convencao, estao anotadas como <i>Entity</i>.
  *
  * @see <a href="https://pt.wikipedia.org/wiki/JavaBeansBEAN">JavaBeans</a>
  * @see <a href="https://pt.wikipedia.org/wiki/MVC">MVC</a>
